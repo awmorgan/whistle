bd339c1f64657c54dced5ce72809967921ae7a5c
Bill Morgan <arthurwilliammorgan@gmail.com>
2023-06-10 11:43:23 -0500

    wip

--- cmd/whistle/datalog.lisp
+++ cmd/whistle/datalog.lisp
@@ -1,9 +1,3 @@
-(define dl_edb (make-hashmap))
-(define dl_idb (make-hashmap))
-(define dl_rdb (quote ()))
-
-(define dl_idx_entity (make-hashmap))
-(define dl_idx_attr (make-hashmap))
 
 (define dl_counter 0)
 (define dl_nextID (lambda () (begin
@@ -13,20 +7,6 @@
 (define dl_assert (lambda (entity attr value) (begin
   (hashmap-set! dl_edb (list entity attr value) #t)
   (dl_update_indices (list entity attr value)))))
-
-(define dl_update_indices (lambda (tuple)
-   (let ((entity (car tuple))
-         (attr (car (cdr tuple))))
-     (let ((m (hashmap-ref dl_idx_entity entity #f)))
-       (if m (hashmap-set! m tuple #t)
-         (let ((new (make-hashmap)))
-           (hashmap-set! dl_idx_entity entity new)
-           (hashmap-set! new tuple #t))))
-     (let ((m (hashmap-ref dl_idx_attr attr #f)))
-       (if m (hashmap-set! m tuple #t)
-         (let ((new (make-hashmap)))
-           (hashmap-set! dl_idx_attr attr new)
-           (hashmap-set! new tuple #t)))))))
 
 (define-syntax dl_record
    (syntax-rules (list let dl_nextID dl_assert)
