6f589405d3242793253273cdd998ef3847b4e554
Bill Morgan <arthurwilliammorgan@gmail.com>
2023-06-10 09:13:10 -0500

    wip

--- lisp/erlang.go
+++ lisp/erlang.go
@@ -11,26 +11,15 @@ type process struct {
 	pid string
 }
 
-type processError struct {
-}
 
 func newProcess() *process {
 	p := &process{
 		pid: pidFunc(),
 	}
-	ch := make(chan SExpression)
-	mailboxes[p.pid] = ch
-	errch := make(chan processError)
-	errchannels[p.pid] = errch
-	processlinks[p.pid] = map[string]struct{}{}
 	go func() {
 	}()
 	return p
 }
-
-var mailboxes = make(map[string]chan SExpression)
-var errchannels = make(map[string]chan processError)
-var processlinks = make(map[string]map[string]struct{})
 
 var pidFunc func() string = generatePid
 
