c00a1185acbc073198f7acb40f1da69b5410ad5b
Bill Morgan <arthurwilliammorgan@gmail.com>
2023-06-10 07:23:44 -0500

    wip

--- cmd/whistle/main.go
+++ cmd/whistle/main.go
@@ -1,13 +1,9 @@
 package main
 
-// http://norvig.com/lispy.html
-
 import (
 	"os"
 
 	"github.com/deosjr/whistle/datalog"
-	"github.com/deosjr/whistle/erlang"
-	"github.com/deosjr/whistle/kanren"
 	"github.com/deosjr/whistle/lisp"
 )
 
@@ -15,8 +11,6 @@ func main() {
 	filename := os.Args[1]
 	sexpressions, _ := lisp.ParseFile(filename)
 	l := lisp.New()
-	kanren.Load(l)
-	erlang.Load(l)
 	datalog.Load(l)
 	for _, e := range sexpressions {
 		l.EvalExpr(e)
