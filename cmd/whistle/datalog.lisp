c316a677b4426e87038cb4d058d9b948ac47ff4a
Bill Morgan <arthurwilliammorgan@gmail.com>
2023-06-10 11:42:05 -0500

    wip

--- cmd/whistle/datalog.lisp
+++ cmd/whistle/datalog.lisp
@@ -33,28 +33,3 @@
      ((_ type (attr value) ...) (let ((id (dl_nextID)))
        (dl_assert id (list type attr) value) ... id))))
 
-(define-syntax dl_find
-   (syntax-rules (where run* equalo dl_edb dl_idb dl_vars let list->set cons eval hashmap-keys fresh dl_findo)
-     ((_ x where ( match ... ))
-      (let ((vars (list->set (dl_vars (quote (x match ...)))))
-            (edb (hashmap-keys dl_edb))
-            (idb (hashmap-keys dl_idb)))
-        (run* (eval (cons 'fresh (cons (cons 'q vars) (quote ((equalo q (quasiquote x)) (dl_findo (quasiquote match) edb idb) ...))))))))))
-
-(define dl_findo (lambda (m edb idb)
-   (fresh (x y entity attr db)
-   (conso entity x m)
-   (conso attr y x)
-     (conde
-       [(boundo entity) (lookupo dl_idx_entity entity db) (membero m db)]
-       [(unboundo entity) (boundo attr) (lookupo dl_idx_attr attr db) (membero m db)] ))))
-
-(define dl_var? (lambda (s) (if (symbol? s) (prefix? (symbol->string s) "?"))))
-
-(define dl_vars (lambda (l)
-   (list->set (foldl (lambda (x acc)
-     (cond
-       [(dl_var? x) (cons x acc)]
-       [(pair? x) (append (dl_vars x) acc)]
-       [else acc])) l (quote ())))))
-
