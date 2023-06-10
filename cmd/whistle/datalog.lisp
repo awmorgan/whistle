bfff7276d7ee0913f6d9cbe536ec219780c58338
Bill Morgan <arthurwilliammorgan@gmail.com>
2023-06-10 11:30:41 -0500

    wip

--- cmd/whistle/datalog.lisp
+++ cmd/whistle/datalog.lisp
@@ -1,30 +1,18 @@
-#| dl_db is a global db to query (not supporting multiple dbs/db as arg)
-   for starters, this will be a simple list of EAV tuples
-   split into EDB (extensional, input), IDB (intentional, derived), and RDB (rules) |#
 (define dl_edb (make-hashmap))
 (define dl_idb (make-hashmap))
 (define dl_rdb (quote ()))
 
-#| further split into entity/attr indices etc to optimise search
-   NOTE: these will include derived tuples |#
 (define dl_idx_entity (make-hashmap))
 (define dl_idx_attr (make-hashmap))
 
-#| dl_nextID is a closure around an ever-increasing counter
-   TODO: proper closure hiding dl_counter instead of this global nonsense |#
 (define dl_counter 0)
 (define dl_nextID (lambda () (begin
    (set! dl_counter (+ dl_counter 1))
    dl_counter)))
 
-#| dl_assert and dl_retract operate on the global edb
-   inserting datoms, 3-value tuples (im ignoring tx, the 4th in EAVT)
-   NOTE: order is thrown out, since we use hashmaps underneath
-   updates entity/attr indices as well (opting not to use a value index yet) |#
 (define dl_assert (lambda (entity attr value) (begin
   (hashmap-set! dl_edb (list entity attr value) #t)
   (dl_update_indices (list entity attr value)))))
-#| TODO: retract |#
 (define dl_update_indices (lambda (tuple)
    (let ((entity (car tuple))
          (attr (car (cdr tuple))))
@@ -39,19 +27,11 @@
            (hashmap-set! dl_idx_attr attr new)
            (hashmap-set! new tuple #t)))))))
 
-#| dl_record is a convenience macro to add structured data
-   returns id of entry added |#
 (define-syntax dl_record
    (syntax-rules (list let dl_nextID dl_assert)
      ((_ type (attr value) ...) (let ((id (dl_nextID)))
        (dl_assert id (list type attr) value) ... id))))
 
-#| NOTE: theres a lot going on in this macro.
-   dl_vars looks for all vars recursively in a list starting with ?
-   when we want to run minikanren we need to explicitly name all vars using 'fresh'
-   NOTE: using more dynamic fresh, not as macro, able to take an interpreted list of varnames
-   if there is a way to do this in the macro system, I havent found it yet (eval hacking instead..)
-   TODO: add unquote to every dl_var instance so we dont have to type it in dl_find input |#
 (define-syntax dl_find
    (syntax-rules (where run* equalo dl_edb dl_idb dl_vars let list->set cons eval hashmap-keys fresh dl_findo)
      ((_ x where ( match ... ))
@@ -60,7 +40,6 @@
             (idb (hashmap-keys dl_idb)))
         (run* (eval (cons 'fresh (cons (cons 'q vars) (quote ((equalo q (quasiquote x)) (dl_findo (quasiquote match) edb idb) ...))))))))))
 
-#| TODO: start at breaking out logic of dl_find macro so we can use indices and speed this up a bit |#
 (define dl_findo (lambda (m edb idb)
    (fresh (x y entity attr db)
    (conso entity x m)
@@ -78,8 +57,6 @@
        [(pair? x) (append (dl_vars x) acc)]
        [else acc])) l (quote ())))))
 
-#| introduce a rule into the (separate!) rule db
-   TODO: only deals with arity/2 predicates atm |#
 (define-syntax dl_rule
    (syntax-rules (list dl_assert_rule :-)
      ((_ (head hx hy) :- (body bx by) ...)
@@ -87,39 +64,25 @@
 
 (define dl_assert_rule (lambda (head body)
   (set! dl_rdb (cons (cons head body) dl_rdb))))
-#| TODO: define dl_retract_rule |#
 
-#| manually trigger fixpoint analysis, repeatedly running rules against all known entities
-   starts by clearing the IDB each time
-   then finds ways to satisfy head of each rule in RDB
-   if this finds any new pairs, repeat, until we reach a fixed point (no more additions)
-   TODO: semi-naive implementation?
-   https://courses.cs.duke.edu/cps216/fall16/Lectures/Lecture-21-Datalog.pdf |#
 (define dl_fixpoint (lambda () (begin
     (set! dl_idb (make-hashmap))
     (dl_fixpoint_iterate))))
 
-#| one iteration of fixpoint analysis |# 
 (define dl_fixpoint_iterate (lambda ()
    (let ((new (hashmap-keys (set_difference (foldl (lambda (x y) (set-extend! y x)) (map dl_apply_rule dl_rdb) (make-hashmap)) dl_idb))))
      (set-extend! dl_idb new)
      (map dl_update_indices new)
      (if (not (null? new)) (dl_fixpoint_iterate)))))
 
-#| TODO: assumes rule head AND each member of body is a predicate of arity/2
-   find all pairs for which rule holds, this can include facts already explored
-   rule is stored in db (and input to this function) as a list of predicates with vars
-   the first item in the list is the head, the rest is the body |#
 (define dl_apply_rule (lambda (rule)
    (let ((head (car rule))
          (body (cdr rule)))
      (eval (quasiquote (dl_find ,head where ,body))))))
 
-#| HELPER FUNCTIONS |#
 (define set-extend! (lambda (m keys)
    (if (null? keys) m (begin (hashmap-set! m (car keys) #t) (set-extend! m (cdr keys))))))
 
-#| NOTE: no 'not' in last clause, so can count double. db semantics probably prevent that |# 
 (define membero (lambda (x l)
    (fresh (a d)
      (equalo (cons a d) l)
@@ -131,7 +94,6 @@
    (if (null? l) acc
      (foldl f (cdr l) (f (car l) acc)))))
 
-#| NOTE: I dont care about order |#
 (define append (lambda (a b)
    (if (null? b) a (append (cons (car b) a) (cdr b)))))
 
@@ -157,7 +119,6 @@
    (check_keys (hashmap-keys a) b))))
 
 (define conso (lambda (a b l) (equalo (cons a b) l)))
-#| NOTE: this breaks logical purity; we can no longer freely reorder goals |#
 (define boundo (lambda (v)
     (lambda (s/c)
       (if (var? v)
@@ -172,7 +133,7 @@
         mzero))))
 (define lookupo (lambda (m key value)
     (lambda (s/c)
-      (let ((k 
+      (let ((k
         (if (var? key)
           (walk key (car s/c))
           key)))
